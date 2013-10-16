6dd1003a49c0d991726d8ac0c6507f701cabc646
Michel Barbou Salvador <mibarbou@gmail.com>
2013-10-16 11:07:14 +0200

    Subida inicial del proyecto Scandal oh  - incluye el archivo oculto .gitignore

---
+++ Scandal oh/Scandal oh/BIZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIZAppDelegate.h
+//  Scandal oh
+//
+//  Created by Michel Barbou Salvador on 10/16/13.
+//  Copyright (c) 2013 Michel Barbou Salvador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Scandal oh/Scandal ohTests/Scandal_ohTests.m
@@ -0,0 +1,34 @@
+//
+//  Scandal_ohTests.m
+//  Scandal ohTests
+//
+//  Created by Michel Barbou Salvador on 10/16/13.
+//  Copyright (c) 2013 Michel Barbou Salvador. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Scandal_ohTests : XCTestCase
+
+@end
+
+@implementation Scandal_ohTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
